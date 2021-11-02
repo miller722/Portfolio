3f39e1980528bcc434dcb2271921b5b1f34d47dc
m.komnazkyi <m.komnazkyi@profermer.com>
2021-11-02 20:10:45 +0200

    jquery

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(window).scroll(function() {
+  $('header').toggleClass('scroll', $(this).scrollTop() > 100);
+});
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
